be7d8bc9905a76afea83bed2aac0a17964b23d0b
gopalakrishnan-r <gopalakrishnars94@gmail.com>
2021-05-06 11:10:11 +0530

    department initial base ready

---
+++ assets/js/arts-science/arts-science.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    var t = $('#example').DataTable();
+    var counter = 1;
+    $.getJSON("/department/arts-science/arts-science.json",
+        function(data) {
+            $.each(data, function(key, value) {
+                var share_url = '<a href="' + value.share + '"><i class="fab fa-whatsapp"></i></a>';
+                t.row.add([
+                //     value.name,
+                    value.dept,
+                    share_url,
+                //     value.intern_category
+                ]).draw(false);
+            });
+        });
+});
