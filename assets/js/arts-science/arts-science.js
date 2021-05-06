1854ecd5ce4560b94e712d10696e8f8b5da68129
gopalakrishnan-r <gopalakrishnars94@gmail.com>
2021-05-06 11:45:16 +0530

    coming soon page addes

--- assets/js/arts-science/arts-science.js
+++ assets/js/arts-science/arts-science.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
     $.getJSON("/department/arts-science/arts-science.json",
         function(data) {
             $.each(data, function(key, value) {
-                var share_url = '<a href="' + value.share + '"><i class="fab fa-whatsapp"></i></a>';
+                var dept_url = '<a href="' + value.default + '">'+value.dept+'</a>';
+                var share_url = '<a href="' + value.share + '"target="_blank"><i class="fab fa-whatsapp"></i></a>';
                 t.row.add([
-                //     value.name,
-                    value.dept,
+                    //     value.name,
+                    dept_url,
                     share_url,
-                //     value.intern_category
+                    //     value.intern_category
                 ]).draw(false);
             });
         });
--- assets/js/engineering/engineering.js
+++ assets/js/engineering/engineering.js
@@ -4,12 +4,13 @@ $(document).ready(function() {
     $.getJSON("/department/engineering/engineering.json",
         function(data) {
             $.each(data, function(key, value) {
-                 var share_url = '<a href="' + value.share + '"><i class="fab fa-whatsapp"></i></a>';
+                var dept_url = '<a href="' + value.default + '">'+value.dept+'</a>';
+                var share_url = '<a href="' + value.share + '"target="_blank"><i class="fab fa-whatsapp"></i></a>';
                 t.row.add([
-                //     value.name,
-                    value.dept,
+                    //     value.name,
+                    dept_url,
                     share_url,
-                //     value.intern_category
+                    //     value.intern_category
                 ]).draw(false);
             });
         });
