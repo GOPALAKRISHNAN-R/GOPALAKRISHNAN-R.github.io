f175a778b93fc60c261313fff97fd1c1fab49820
gopalakrishnan-r <gopalakrishnars94@gmail.com>
2021-05-06 14:21:00 +0530

    whatsapp share added

--- assets/js/arts-science/arts-science.js
+++ assets/js/arts-science/arts-science.js
@@ -5,7 +5,9 @@ $(document).ready(function() {
         function(data) {
             $.each(data, function(key, value) {
                 var dept_url = '<a href="' + value.default + '">'+value.dept+'</a>';
-                var share_url = '<a href="' + value.share + '"target="_blank"><i class="fab fa-whatsapp"></i></a>';
+                var share_url = '<a href="' + value.share + '"target="_blank" data-action="share/whatsapp/share"><i class="fab fa-whatsapp"></i></a>';
+                // var share_url = '<a href="' + value.share + '"target="_blank"><i class="fab fa-whatsapp"></i></a>';
+                // <a href="whatsapp://send?text=The text to share!" data-action="share/whatsapp/share">Share via Whatsapp</a>
                 t.row.add([
                     //     value.name,
                     dept_url,
