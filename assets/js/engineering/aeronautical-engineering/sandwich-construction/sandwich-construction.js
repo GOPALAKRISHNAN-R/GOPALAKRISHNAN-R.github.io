9baff6b5b2823911e398dc68d107a39ffa865cf2
gopalakrishnan-r <gopalakrishnars94@gmail.com>
2021-05-07 13:23:18 +0530

    two topics added in aeronautical engineering

---
+++ assets/js/engineering/aeronautical-engineering/sandwich-construction/sandwich-construction.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+    $.getJSON("/department/engineering/aeronautical-engineering/sandwich-construction/sandwich-construction.json",
+
+        function(data) {
+            var topic = '';
+            var topic_head = '';
+            topic_head += '<h3 class="text-uppercase text-center">' + data[0].topic + '</h3>'
+            topic_head += '<hr class="divider">'
+            topic_head += '<p class="text-center">' + data[0].def + '</p>'
+            $('#topic-details-head').append(topic_head);
+
+            $.each(data, function(key, value) {
+
+
+                topic += '<div class="col-lg-12 col-md-6 col-sm-6 ">'
+                topic += '<div class="mt-5 ">'
+                topic += '<h4 class="text-muted mt-2">' + value.heading + '</h4>'
+                topic += '<hr class="divider ml-0">'
+                topic += '<p class="mt-2">' + value.notes + '</p>'
+                topic += '</div>'
+                topic += '</div>'
+
+
+
+            });
+
+            $('#topic-details').append(topic);
+
+        });
+});
