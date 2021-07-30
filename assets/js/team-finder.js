4fb22a808496e052856971149b139ad53784906f
gopalakrishnan-r <rgopalakrishnanmca@gmail.com>
2021-07-31 00:11:18 +0530

    team finder part 1 partially completed

--- assets/js/team-finder.js
+++ assets/js/team-finder.js
@@ -1,29 +1,23 @@
-$(document).ready(function() {
-
-
+$(function() {
     $.getJSON("/office-activity/team.json",
 
         function(data) {
-
             let teams = [];
             let participant = '';
 
             var team = '';
-            var participantList = '';
+            var participantList = [];
 
             var count = 0;
             let selectedCount = 0;
 
             var totalTeam = '';
-            var selectedTeamMembers = '';
+            var selectedTeamMembers = [];
 
-            $("#btnAdd").click(function() {
-                participant = $("#participant").val();
-                if (participant != "") {
-                    $("#team-container").prepend('<option>' + participant + '</option>')
-                    $("#participant").val('');
-                }
-            });
+            var totalSelectedTeamMembers = [];
+
+            $("#viewScoreBoardDetails").hide();
+            $("#hideScoreBoard").hide();
 
             $.each(data, function(key, value) {
                 if (value.active) {
@@ -36,27 +30,65 @@ $(document).ready(function() {
                 teams = $("#team-container option").filter(":selected").text();
             });
 
-            $("#moveRight").click(function() {
-                participantList = '<option>' + teams + '</option>'
-                selectedCount += 1;
-                $('#selectedListParticipants').append(participantList);
-            });
-
             $("#selectedListParticipants").click(function() {
                 teams = $("#selectedListParticipants option").filter(":selected").text();
             });
 
-            $("#btnDelete").click(function() {
-                $("#selectedListParticipants option").filter(":selected").remove();
-            });
 
             totalTeam += '<label for="exampleFormControlSelect2">Select the participants (' +
                 count + ')</label>'
+            selectedTeamMembers = '<label for="exampleFormControlSelect2">Selected participants (' + selectedCount + ')</label>'
 
+            $('#selectedTeamMembers').append(selectedTeamMembers);
             $('#team-container').append(team);
             $('#totalTeamMembers').append(totalTeam);
-            $('#selectedTeamMembers').append(selectedTeamMembers);
+
+
+            $("body").click(function(event) {
+                let target = event.target.id;
+                switch (target) {
+                    case "btnAdd":
+                        add();
+                        break;
+                    case "moveRight":
+                        moveRight(teams);
+                        break;
+                    case "btnDelete":
+                        deleteParticipant();
+                        break;
+                    case "viewScoreBoard":
+                        $("#viewScoreBoard").hide();
+                        $("#hideScoreBoard").show();
+                        $("#viewScoreBoardDetails").show();
+                        break;
+                    case "hideScoreBoard":
+                        $("#viewScoreBoard").show();
+                        $("#hideScoreBoard").hide();
+                        $("#viewScoreBoardDetails").hide();
+                }
+            });
+
+            function add() {
+                participant = $("#participant").val();
+                if (participant != "") {
+                    $("#team-container").prepend('<option>' + participant + '</option>')
+                    $("#participant").val('');
+                }
+            }
+
+            function moveRight(teams) {
+                if (teams != '') {
+                    participantList = '<option>' + teams + '</option>'
+                    selectedCount += 1;
+                }
+
+                $('#selectedListParticipants').append(participantList);
+
+            }
+
+            function deleteParticipant() {
+                $("#selectedListParticipants option").filter(":selected").remove();
+            }
 
         });
-
 });
