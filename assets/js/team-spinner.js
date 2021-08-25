d57c81fdc3b641cec9147b9d357027b7a9e1016c
gopalakrishnan-r <rgopalakrishnanmca@gmail.com>
2021-08-25 22:15:40 +0530

    spinner ui changed

--- assets/js/team-spinner.js
+++ assets/js/team-spinner.js
@@ -15,22 +15,26 @@ $(function() {
         });
 
     function result(selectedTeamMembers) {
-        var displayResult = '';
         $("#spin").click(function() {
-            var item = selectedTeamMembers[Math.floor(Math.random() * selectedTeamMembers.length)];
-            displayResult += '<div class="callout">'
-            displayResult += '<div class="callout-header">Congratulations</div>'
-            displayResult += '<span class="closebtn" onclick="this.parentElement.style.display=`none`;location.reload();">×</span>'
-            displayResult += '<div class="callout-container">'
-            displayResult += '<p id="result"></p>'
-            displayResult += '<p>You got selected!!</p>'
-            displayResult += ' </div>'
-            displayResult += '</div>'
-            // displayResult += '<div class="alert alert-success" role="alert" id="result">'
-            // displayResult += '</div>'
-            $('#winner-result').append(displayResult);
-            $("#result").html(item);
+            $('.image').toggleClass('image-active');
+            window.setTimeout(function() {
+                resultDisplay(selectedTeamMembers);
+            }, 4000);
         });
+    }
 
+    function resultDisplay(selectedTeamMembers) {
+        var displayResult = '';
+        var item = selectedTeamMembers[Math.floor(Math.random() * selectedTeamMembers.length)];
+        displayResult += '<div class="callout">'
+        displayResult += '<div class="callout-header">Congratulations</div>'
+        displayResult += '<span class="closebtn" onclick="this.parentElement.style.display=`none`;location.reload();">×</span>'
+        displayResult += '<div class="callout-container">'
+        displayResult += '<p id="result"></p>'
+        displayResult += '<p>You got selected!!</p>'
+        displayResult += ' </div>'
+        displayResult += '</div>'
+        $('#winner-result').append(displayResult);
+        $("#result").html(item);
     }
 });
