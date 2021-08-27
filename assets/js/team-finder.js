0cae1b5dff393f397f4f10cfd2ec61f4d66d6b71
gopalakrishnan-r <rgopalakrishnanmca@gmail.com>
2021-08-27 15:32:07 +0530

    team finder file updated

--- assets/js/team-finder.js
+++ assets/js/team-finder.js
@@ -14,7 +14,12 @@ var totalSelectedTeamMembers = [];
 
 var generateTeamList = '';
 
+var resultIndex = [];
+
 var result = 0;
+
+
+var pId;
 
 $(function() {
     $.getJSON("/office-activity/team.json",
@@ -34,21 +39,24 @@ $(function() {
                 }
             });
 
+
+
+
             $("body").click(function(event) {
                 let target = event.target.id;
+
+                pId = event.target.parentElement.id;
                 switch (target) {
                     case "selectTeam":
                         selectedTeamMembersFromList();
                         break;
-                    case "addBtn":
-                        result += 5;
-                        // $("#sum").html(result);
-                        outputResult(result);
+                    case "addBtn" + pId:
+                        var point = 5;
+                        add(point, pId);
                         break;
-                    case "subBtn":
-                        result -= 5;
-                        // $("#sum").html(result);
-                        outputResult(result);
+                    case "subBtn" + pId:
+                        var point = 5;
+                        sub(point, pId);
                         break;
                     case "btnGenerate":
                         generateTeam();
@@ -65,6 +73,20 @@ $(function() {
                 }
             });
 
+            function add(point, pId) {
+                result = result + point;
+                console.log(pId + "===" + result + "=" + result + "+" + point);
+                $("#sum" + pId).html(result);
+            }
+
+            function sub(point, pId) {
+                result = result - 5;
+                console.log(pId + "===" + result + "=" + result + "+" + point);
+                $("#sum" + pId).html(result);
+            }
+
+
+
 
             function selectedTeamMembersFromList() {
                 // body...
@@ -72,14 +94,10 @@ $(function() {
                     return $(el).val();
                 }).get();
                 $.each(selectedTeamMembers, function(index, value) {
-
                     participantList += '<option>' + (index + 1) + ') ' + value + '</option>'
-
                     count = index + 1;
                 });
 
-                // var item = selectedTeamMembers[Math.floor(Math.random() * selectedTeamMembers.length)];
-                // alert(item)
                 $('#selected-team').append(participantList);
 
             }
@@ -93,11 +111,10 @@ $(function() {
                         generateTeamList += '<div class="card">'
                         generateTeamList += '<h5 class="card-header">' + value + '</h5>'
                         generateTeamList += '<div class="card-body">'
-                        generateTeamList += '<p class="card-text"><button type="button" class="btn btn-success" id="addBtn">+5</button><button type="button" class="btn btn-danger" id="subBtn">-5</button> = <span id="sum"></span></p>'
+                        generateTeamList += '<p id=' + index + ' class="card-text"><button type="button" class="btn btn-success" id="addBtn' + index + '">+5</button><button type="button" class="btn btn-danger" id="subBtn' + index + '">-5</button> = <span id="sum' + index + '"></span></p>'
                         generateTeamList += '</div>'
                         generateTeamList += '</div>'
                         generateTeamList += '</div>'
-
                     });
                     $('#generate-card-team').append(generateTeamList);
                 } else {
@@ -105,6 +122,7 @@ $(function() {
                 }
 
             }
+
 
             function chunkArray(arr, n) {
 
@@ -117,7 +135,7 @@ $(function() {
                     // console.log(i + "array " + chunks[i]);
                     generateTeamList += '<div class="col-lg-6 col-md-12 col-sm-12">'
                     generateTeamList += '<div class="card">'
-                    generateTeamList += '<h5 class="card-header">Team ' + (i + 1) + '<span><button type="button" class="btn btn-success" id="addBtn">+5</button><button type="button" class="btn btn-danger" id="subBtn">-5</button> = <span id="sum"></span></span></h5>'
+                    generateTeamList += '<h5 class="card-header">Team ' + (i + 1) + '<span id='+i+'><button type="button" class="btn btn-success" id="addBtn' + i+ '">+5</button><button type="button" class="btn btn-danger" id="subBtn' + i + '">-5</button> = <span id="sum' + i + '"></span></span></h5>'
 
                     generateTeamList += '<div class="card-body">'
                     generateTeamList += '<p class="card-text">' + chunks[i] + '</p>'
@@ -131,10 +149,6 @@ $(function() {
             }
 
 
-            function outputResult(result) {
-                 $("#sum").html(result);
-                // return result;
-            }
             // function teamGenerate(count, countvalue) {
             //     var teamCount = count / countvalue;
             //     console.log(count + ':' + countvalue);
