1df025fef2060a4866de392de621fea7b042e30d
gopalakrishnan-r <rgopalakrishnanmca@gmail.com>
2021-09-13 11:23:39 +0530

    office activity page updated

--- assets/js/office-activity.js
+++ assets/js/office-activity.js
@@ -9,17 +9,18 @@ $(document).ready(function() {
                 activity += '<div class="mt-5 shadow p-3 mb-5 bg-white rounded zoom mr-3 text-center">'
                 activity += '<span class="fa-stack fa-4x">'
                 if (value.activity == "Expense Claim") {
-                    activity += '<i class="fas fa-circle fa-stack-2x text-primary"></i>'
+                    activity += '<i class="fas fa-square fa-stack-2x text-color-cyan"></i>'
                     activity += '<i class="fas fa-file-invoice fa-stack-1x fa-inverse"></i>'
                 } else if (value.activity == "Team Finder") {
-                    activity += '<i class="fas fa-circle fa-stack-2x text-primary"></i>'
+                    activity += '<i class="fas fa-square fa-stack-2x text-color-cyan"></i>'
                     activity += '<i class="fas fa-users fa-stack-1x fa-inverse"></i>'
                 } else if (value.activity == "Spinner") {
-                    activity += '<i class="fas fa-circle fa-stack-2x text-primary"></i>'
+                    activity += '<i class="fas fa-square fa-stack-2x text-color-cyan"></i>'
                     activity += '<i class="fas fa-gamepad fa-stack-1x fa-inverse"></i>'
                 }
                 activity += '</span><br><br>'
-                activity += '<h4 class="text-primary text-center"><a href="' + value.link + '">' + value.activity + '</a>' + '</h4>'
+                activity += '<h4 class="text-center"><a href="' + value.link + '">'
+                activity += '<span class="text-color-dark text-uppercase">' + value.activity + '</span></a>' + '</h4>'
                 activity += '<br>'
                 activity += '</div>'
                 activity += '</div>'
