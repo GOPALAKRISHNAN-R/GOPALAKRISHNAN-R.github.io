92544cd233152895059a6db5311b441b3c7965f2
gopalakrishnan-r <rgopalakrishnanmca@gmail.com>
2021-12-07 15:08:29 +0530

    title font face updated

--- assets/js/products.js
+++ assets/js/products.js
@@ -1,6 +1,8 @@
 /*
+    @author R.Gopalakrishnan
     Product json Developed on 26/10/2021.
 */
+
 $(document).ready(function() {
 
     $.getJSON("/assets/data/products.json",
@@ -12,13 +14,15 @@ $(document).ready(function() {
             $.each(data, function(key, value) {
 
                 indexProductList += '<div class="col-lg-3 col-md-6 text-center mt-2">'
+
                 indexProductList += '<div class="card" style="width: 16rem;">'
                 indexProductList += '<a href="' + value.url + '" >'
-                indexProductList += '<img class="card-img-top" src="' + value.imageUrl + '" alt="Card image cap">'
+                indexProductList += '<img class="card-img-top" src="'
+                indexProductList += value.imageUrl
+                indexProductList += '" alt="Card image cap">'
                 indexProductList += '</a>'
-                indexProductList += '<div class="card-body">'
-                // indexProductList += '<a href="' + value.url + '" >'
 
+                indexProductList += '<div class="card-body">'
                 indexProductList += '<h5 class="card-title" >'
                 indexProductList += '<a href="' + value.url + '" >'
                 indexProductList += key
@@ -29,19 +33,15 @@ $(document).ready(function() {
                 indexProductList += '</span><strong> '
                 indexProductList += value.price
                 indexProductList += '</strong></p>'
-                // indexProductList += '</a>'
-
                 indexProductList += '<a href="' + value.buy + '" ><button>Buy</button></a>'
                 indexProductList += '</div>'
+
                 indexProductList += '</div>'
+
                 indexProductList += '</div>'
 
             });
 
-            //tornnado
-            // if (key == "Tornnado") {
-
-            // }
 
             $('#products-list').append(indexProductList);
         });
